d2c441a43b8e6e13c27364a9707997617efbf8c2
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-29 11:46:11 -0400

    advanced deliverables done

--- src/index.js
+++ src/index.js
@@ -3,34 +3,70 @@
 fetch ramens from server
 parse response
 for each ramen, create an image element, set its source to ramen image, and append to ramen menu
+in my last .then, run addrameninfo with first ramen in ramens array
 */
+const ratingDisplay = document.querySelector("#rating-display");
+const commentDisplay = document.querySelector("#comment-display");
+const detailImg = document.querySelector(".detail-image");
 
 fetch("http://localhost:3000/ramens")
   .then((res) => res.json())
-  .then((ramens) => ramens.forEach(showImages));
+  .then((ramens) => {
+    ramens.forEach(showImages);
+    addRamenInfo(null, ramens[0]);
+  });
 
 function showImages(ramen) {
   const img = document.createElement("img");
   img.src = ramen.image;
   img.addEventListener("click", (event) => addRamenInfo(event, ramen));
-  document.getElementById("ramen-menu").append(img);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  document.getElementById("ramen-menu").append(deleteBtn, img);
+  deleteBtn.addEventListener("click", (event) =>
+    deleteRamen(event, ramen, img)
+  );
 }
 
 /*add event listener to each image above
 consider passing ramen into event listener callback
 in the callback, grab the elements, set the inner texts to their respective ramen properties
-*/
+add a delete button in ramen detail div
+add event listener for delete button that calls a function
+the function will remove the details from ramen detail div, as well as from ramen menu*/
 function addRamenInfo(event, ramen) {
   const image = document.querySelector(".detail-image");
   image.src = ramen.image;
+  image.id = ramen.id;
   const name = document.querySelector(".name");
   name.innerText = ramen.name;
   const restaurant = document.querySelector(".restaurant");
   restaurant.innerText = ramen.restaurant;
-  const rating = document.querySelector("#rating-display");
-  rating.innerText = ramen.rating;
-  const comment = document.querySelector("#comment-display");
-  comment.innerText = ramen.comment;
+  ratingDisplay.innerText = ramen.rating;
+  commentDisplay.innerText = ramen.comment;
+}
+
+function deleteRamen(event, ramen, img) {
+  if (img.src === detailImg.src) {
+    detailImg.src = "./assets/image-placeholder.jpg";
+    const detailDiv = document.querySelector("#ramen-detail");
+    detailDiv.querySelector("h2").innerText = "Insert Name Here";
+    detailDiv.querySelector("h3").innerText = "Insert Restaurant Here";
+    ratingDisplay.innerText = "Insert rating here";
+    commentDisplay.innerText = "Insert comment here";
+  }
+  event.target.remove();
+  img.remove();
+  const configObj = {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  fetch(`http://localhost:3000/ramens/${ramen.id}`, configObj)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
 
 /*create event listener for submit which calls a function to add info to menu
@@ -38,7 +74,7 @@ create variable for each ramen property, grab the value from each input, then se
 create object using those properties
 run show images with new ramen we made
 */
-document
+const newRamenForm = document
   .getElementById("new-ramen")
   .addEventListener("submit", (event) => createRamen(event));
 function createRamen(event) {
@@ -46,8 +82,8 @@ function createRamen(event) {
   const name = document.querySelector("#new-name").value;
   const restaurant = document.querySelector("#new-restaurant").value;
   const image = document.querySelector("#new-image").value;
-  const rating = document.querySelector("#new-rating").value;
-  const comment = document.querySelector("#new-comment").value;
+  const rating = event.target.querySelector("#new-rating").value;
+  const comment = event.target.querySelector("#new-comment").value;
   const newRamen = {
     name: name,
     restaurant: restaurant,
@@ -55,5 +91,40 @@ function createRamen(event) {
     rating: rating,
     comment: comment,
   };
+  const configObj = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newRamen),
+  };
+  fetch(`http://localhost:3000/ramens`, configObj);
   showImages(newRamen);
+}
+
+/*add event listener for submitting edit form
+grab values from both input fields
+set inner text of comment and rating elements to the values from above*/
+document.querySelector("#edit-ramen").addEventListener("submit", editRamen);
+function editRamen(event) {
+  event.preventDefault();
+  const rating = event.target.querySelector("#new-rating").value;
+  const comment = event.target.querySelector("#new-comment").value;
+  document.querySelector("#rating-display").innerText = rating;
+  document.querySelector("#comment-display").innerText = comment;
+  const configObj = {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      rating: rating,
+      comment: comment,
+    }),
+  };
+  fetch(`http://localhost:3000/ramens/${detailImg.id}`, configObj)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
